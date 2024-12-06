98367a948ec81d1223ecd114a1ff1ba31e0dc70c
caleb levy <buntulevycaleb@gmail.com>
2024-12-07 00:35:44 +0200

    fix fot the\ deploy my Portfolio

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/desc-softlab-digital-solutions/', 
+  base: './', 
   server: {
     host: "::",
     port: 8080,
@@ -23,5 +23,6 @@ export default defineConfig(({ mode }) => ({
   build: {
     outDir: 'dist', 
     assetsDir: 'assets',
+    sourcemap: true, 
   },
 }));
